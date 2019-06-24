be62a0936ab42be1afd46c41e279009b3756dbec
Artem Babich (DevExpress) <babich.artem@corp.devexpress.com>
2019-06-24 15:11:56 +0300

    add example content

---
+++ CS/WebSite/App_Code/DataModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+public class DataModel {
+    const string SessionKey = "Items";
+    public void SaveToSession(List<Item> items) {
+        HttpContext.Current.Session[SessionKey] = items;
+    }
+    public List<Item> GetItems() {
+        if (HttpContext.Current.Session[SessionKey] == null) {
+            GenerateItems();
+        }
+        return (List<Item>)HttpContext.Current.Session[SessionKey];
+    }
+    public void InsertItem(Item item) {
+        List<Item> items = GetItems();
+        int id = GetNewID();
+        item.ID = id;
+        items.Add(item);
+        SaveToSession(items);
+    }
+    public void RemoveItem(Item item) {
+        List<Item> items = GetItems();
+        items.RemoveAll(c => c.ID == item.ID);
+        SaveToSession(items);
+    }
+
+    public void UpdateItem(Item newItemData) {
+        List<Item> items = GetItems();
+        int index = items.IndexOf(items.First(s => s.ID == newItemData.ID));
+        items[index] = newItemData;
+        SaveToSession(items);
+    }
+
+    void GenerateItems() {
+        List<Item> items = new List<Item>();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                Item item = new Item();
+                item.ID = i * 5 + j;                
+                item.TypeID = i;
+                item.SubTypeID = i * 10 + j;
+                items.Add(item);
+            }
+        }
+        SaveToSession(items);
+    }
+    int GetNewID() {
+        List<Item> items = GetItems();
+        int maxID = items.Max(s => s.ID);
+        return maxID + 1;
+    }
+}
+
+public static class ItemTypeFactory {
+    public static List<ItemType> GetItemTypes() {
+        List<ItemType> itemTypes = new List<ItemType>();
+        for (int i = 0; i < 10; i++) {
+            ItemType itemType = new ItemType();
+            itemType.TypeID = i;
+            itemType.TypeName = String.Format("Type: {0}", i);
+
+            itemTypes.Add(itemType);
+        }
+        return itemTypes;
+    }
+    public static List<SubType> GetItemSubTypes() {
+        List<SubType> itemSubTypes = new List<SubType>();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                SubType subType = new SubType();
+                subType.TypeID = i;
+                subType.SubTypeID = i * 10 + j;
+                subType.SubTypeName = String.Format("SubType: {0:00}", i * 10 + j);
+                subType.SubTypeDescription = String.Format("SubType description {0}", i * j + j);
+                itemSubTypes.Add(subType);
+            }
+        }
+        return itemSubTypes;
+    }
+}
+
+public class Item {
+    public int ID { get; set; }
+    public int TypeID { get; set; }
+    public int SubTypeID { get; set; }
+}
+
+public class ItemType {
+    public int TypeID { get; set; }
+    public string TypeName { get; set; }
+}
+public class SubType {
+    public int TypeID { get; set; }
+    public int SubTypeID { get; set; }
+    public string SubTypeName { get; set; }
+    public string SubTypeDescription { get; set; }
+}
---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using DevExpress.Web;
+using DevExpress.Web.Bootstrap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!IsPostBack && !IsCallback)
+            Session.Clear();
+        ((BootstrapGridViewComboBoxColumn)BootstrapGridView1.Columns["TypeID"]).PropertiesComboBox.DataSource = ItemTypeFactory.GetItemTypes();
+        BootstrapGridView1.DataSource = objectDataSource1;
+        BootstrapGridView1.DataBind();
+
+    }
+    protected void OnItemRequestedByValue_View(object source, ListEditItemRequestedByValueEventArgs e) {
+        if (e.Value == null) return;
+        int subTypeID = (int)e.Value;
+        BootstrapComboBox editor = source as BootstrapComboBox;
+        editor.DataSource = ItemTypeFactory.GetItemSubTypes().Where(s => s.SubTypeID == subTypeID);
+        editor.DataBind();
+    }
+    protected void OnItemsRequestedByFilterCondition_View(object source, ListEditItemsRequestedByFilterConditionEventArgs e) {
+        BootstrapComboBox editor = source as BootstrapComboBox;
+        editor.DataSource = ItemTypeFactory.GetItemSubTypes().Where(s => s.SubTypeDescription.Contains(e.Filter)
+        || s.SubTypeName.Contains(e.Filter)
+        || s.SubTypeID.ToString().Contains(e.Filter));
+        editor.DataBind();
+    }
+    protected void OnItemRequestedByValue(object source, ListEditItemRequestedByValueEventArgs e) {
+        if (e.Value == null) return;
+        string id = e.Value.ToString();
+        BootstrapComboBox editor = source as BootstrapComboBox;
+        int typeID = GetCurrentItemTypeID();
+        List<SubType> subTypes;
+        if (typeID > -1)
+            subTypes = ItemTypeFactory.GetItemSubTypes().Where(s => s.TypeID == typeID && s.SubTypeID.ToString() == id).ToList();
+        else
+            subTypes = ItemTypeFactory.GetItemSubTypes().Where(s => s.TypeID == typeID).ToList();
+        editor.DataSource = subTypes;
+        editor.DataBind();
+    }
+
+    protected void OnItemsRequestedByFilterCondition(object source, ListEditItemsRequestedByFilterConditionEventArgs e) {
+        BootstrapComboBox editor = source as BootstrapComboBox;
+        int typeID = GetCurrentItemTypeID();
+        List<SubType> subTypes;
+        if (typeID > -1)
+            subTypes = ItemTypeFactory.GetItemSubTypes().Where(s => s.TypeID == typeID && (s.SubTypeDescription.Contains(e.Filter)
+        || s.SubTypeName.Contains(e.Filter)
+        || s.SubTypeID.ToString().Contains(e.Filter))).ToList();
+        else
+            subTypes = ItemTypeFactory.GetItemSubTypes().Where(s => s.SubTypeDescription.Contains(e.Filter)
+        || s.SubTypeName.Contains(e.Filter)
+        || s.SubTypeID.ToString().Contains(e.Filter)).ToList();
+        editor.DataSource = subTypes;
+        editor.DataBind();
+    }
+    public int GetCurrentItemTypeID() {
+        object typeID;
+        if (ASPxHiddenField1.TryGet("currentTypeID", out typeID))
+            return Convert.ToInt32(typeID);
+        return -1;
+    }
+}
